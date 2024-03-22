80453c9287998056b5a13568953031f7a426ddea
timone019 <143154960+timone019@users.noreply.github.com>
2024-03-21 20:33:25 -0700

    removed duplicate unrefactored scenario 1 test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -16,20 +16,7 @@ describe('show/hide event details', () => {
   afterAll(() => {
     browser.close();
   });
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    // if your event element has a different selector, use it instead of .event
-    await page.waitForSelector('.event');
-
-    // if your event's details have a different selector, use it instead of .event .details
-    const eventDetails = await page.$('.event .details');
-    expect(eventDetails).toBeNull();
-    browser.close();
-  });
   // Scenario 1
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .details');
@@ -53,19 +40,7 @@ describe('show/hide event details', () => {
 });
 
 
-//         const browser = await puppeteer.launch();
-    
-//         const page = await browser.newPage();
-//         await page.goto('http://localhost:3000/');
-    
-//         // if your event element has a different selector, use it instead of .event
-//         await page.waitForSelector('.event');
-    
-//         // if your event's details have a different selector, use it instead of .event .details
-//         const eventDetails = await page.$('.event .details');
-//         expect(eventDetails).toBeNull();
-//         browser.close();
-//       });
+
 
         // test('An event element is collapsed by default', async () => {
         //     const browser = await puppeteer.launch();
