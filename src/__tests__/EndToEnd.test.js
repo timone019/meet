d18b311725644905219b8a885a985c281bbd0933
timone019 <143154960+timone019@users.noreply.github.com>
2024-03-21 20:27:54 -0700

    fixed syntax error - end quote

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -16,7 +16,20 @@ describe('show/hide event details', () => {
   afterAll(() => {
     browser.close();
   });
+  test('An event element is collapsed by default', async () => {
+    const browser = await puppeteer.launch();
 
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    // if your event element has a different selector, use it instead of .event
+    await page.waitForSelector('.event');
+
+    // if your event's details have a different selector, use it instead of .event .details
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeNull();
+    browser.close();
+  });
   // Scenario 1
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .details');
