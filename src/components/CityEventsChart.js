130e0db5955ee3f8bec581e80e0e1b901b1af29e
timone019 <143154960+timone019@users.noreply.github.com>
2024-04-02 14:14:04 -0700

    4.10 restricted Decimals to false on chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -41,7 +41,7 @@ const CityEventsChart = ({ allLocations, events }) => {
       >
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="City" /> 
-        <YAxis type="number" dataKey="count" name="Number of events" /> 
+        <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} /> 
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
