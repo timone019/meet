34658df89e1a096806469e95b9974ebbe70818d0
timone019 <143154960+timone019@users.noreply.github.com>
2024-04-02 14:08:16 -0700

    4.10 updated useEffect array dependency from data to events to fix blank reload of charts

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,24 +1,33 @@
 // src/components/CityEventsChart.js
 
-import { useState, useEffect } from 'react';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { useState, useEffect } from "react";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const CityEventsChart = ({ allLocations, events }) => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setData(getData()); // eslint-disable-next-line
-    }, [`${data}`]);
+  useEffect(() => {
+    setData(getData()); // eslint-disable-next-line
+  }, [`${events}`]);
 
-    const getData = () => {
-        const data = allLocations.map((location) => {
-            const count = events.filter((event) => event.location === location).length
-            const city = location.split(', ')[0]
-            return { city, count };
-        })
-        return data;
-    };
-
+  const getData = () => {
+    const data = allLocations.map((location) => {
+      const count = events.filter(
+        (event) => event.location === location
+      ).length
+      const city = location.split(", ")[0]
+      return { city, count };
+    })
+    return data;
+  };
 
   return (
     <ResponsiveContainer width="99%" height={400}>
@@ -31,13 +40,13 @@ const CityEventsChart = ({ allLocations, events }) => {
         }}
       >
         <CartesianGrid />
-        `<XAxis type="category" dataKey="city" name="City" />`
-        `<YAxis type="number" dataKey="count" name="Number of events" />`
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <XAxis type="category" dataKey="city" name="City" /> 
+        <YAxis type="number" dataKey="count" name="Number of events" /> 
+        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
   );
-}
+};
 
-export default CityEventsChart
+export default CityEventsChart;
